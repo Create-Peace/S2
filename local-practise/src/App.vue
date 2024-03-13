a8d32b3fc357573da6c86e012c54bf436b6685b5
M-Ray <zhaomm@shuhaisc.com>
2024-03-13 10:33:38 +0800

    style: table

--- local-practise/src/App.vue
+++ local-practise/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <div ref="s2Container"></div>
+    <div ref="s2Container" style="width: 800px; margin: auto"></div>
   </div>
 </template>
 
