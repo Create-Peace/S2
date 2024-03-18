7f332ca4c53fbc209dcfa3010ed2c7bf46410738
M-Ray <zhaomm@shuhaisc.com>
2024-03-18 15:21:06 +0800

    feat: transfer yarn to pnpm

--- local-practise/src/customCell/CustomColCell.js
+++ local-practise/src/customCell/CustomColCell.js
@@ -1,39 +1,27 @@
-import { ColCell, EXTRA_FIELD } from '../../packages/s2-core'
-import { Polyline } from '@antv/g';
+import { ColCell } from '../../../packages/s2-core'
+import { Line } from '@antv/g';
 export default class CustomColCell extends ColCell {
-  // 覆盖背景绘制，可覆盖或者增加绘制方法
-  getBackgroundColor () {
-    let bg = null
-    if (this.meta.field === EXTRA_FIELD && this.tableColumnBg?.[this.meta.query?.[this.colDimensionAlias?.[0]]]) {
-      bg = this.tableColumnBg[this.meta.query[this.colDimensionAlias[0]]]?.colorValue
-      return {
-        backgroundColor: bg || '#fff'
-      }
+
+  initCell () {
+    super.initCell()
+    this.drawBorder()
+  }
+
+  drawBorder() {
+  
+    this.appendChild(
+        new Line({
+          style: {
+            x1: this.meta.x,
+            y1: this.meta.y,
+            x2: this.meta.x,
+            y2: this.meta.y + this.meta.height,
+            stroke: '#37f',
+            lineWidth: 4,
+            zIndex: 999999,
+          }
+        })
+      )
     }
-    return super.getBackgroundColor()
-  }
-
-  drawBackgroundShape() {
-    /* if (this.meta.rowIndex > 0) {
-      return super.drawBackgroundShape();
-    } */
-
-    this.backgroundShape = this.appendChild(
-      new Polyline({
-        style: {
-          points: [
-            [this.meta.x, this.meta.y],
-            [this.meta.x, this.meta.y + this.meta.height],
-           /*  [this.meta.x + this.meta.width, this.meta.y + this.meta.height],
-            [this.meta.x + this.meta.width, this.meta.y],
-            [this.meta.x, this.meta.y] */
-          ],
-          stroke: '#379',
-          lineWidth: 2,
-          // zIndex: 999,
-        },
-      }),
-    );
-  }
 
 }
